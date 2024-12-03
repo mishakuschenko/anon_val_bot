f8afe3ef9a4af3531749108d9dc6d4c4a2363c6d
mishakuschenko <liahimkusenko@gmail.com>
2024-12-04 00:22:02 +0300

    .

--- lib/utils.py
+++ lib/utils.py
@@ -1,8 +1,5 @@
 class ReadText:
     def get_text(self, path: str) -> str:
-        try:
-            with open(path) as file:
-                text = file.read()
-                return text 
-        except FileNotFoundError:
-            return False
+        with open(path) as file:
+            text = file.read()
+            return text 
