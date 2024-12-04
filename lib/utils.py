72630372dde7f1d4c7d2a7469546102c6363bc5c
Dnkind <deniskin837@gmail.com>
2024-12-04 20:09:13 +0300

    .

--- lib/utils.py
+++ lib/utils.py
@@ -3,3 +3,8 @@ class ReadText:
         with open(path) as file:
             text = file.read()
             return text 
+
+mas_of_id = []
+def tme_url(id):
+    mas_of_id.append(id)
+    return "https://t.me/anon_message_ru_bot?start=" + str(id)
